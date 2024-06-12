43eab6648798815c88d2b59ea41aceaf4158f7ca
just.acat <95175899+Jusxern@users.noreply.github.com>
2024-06-12 11:28:46 +0500

    Update vm.lua

--- vm.lua
+++ vm.lua
@@ -5,50 +5,58 @@ local vmlua = require('iov')
 vmlua.bridge('VMLuaJAV')
 local VMcode = [[
     local VMF = require'iov'.connect('VMLuaJAV').data.VMF
-    local require = VMF.require
-    local VMio = require('io')
+
     local computer = VMF.computer
-    local govno = 'CHLEN'
+    local require = VMF.require
+    local pcall = VMF.pcall
+   
+    computer.shutdown(true)
+
     print('\nGuest: ',computer.address())
-    pcall(function()
-    --parasha 
-    end)
 ]]
 local code = [[
-    print(govno)
+    
 ]]
 local UUIDS = {}
 local Calls = {}
 for i=1,100 do table.insert(UUIDS, uuid.next()) end
 
-local VMF = {
-    require = function(module) table.insert(Calls,{'require',module}) return require(module) end;
-    computer = {
-        ProgramLoactions = {};
-        address = function() return UUIDS[1] end;
-        addUser = function(name) return 1 end;
-        beep = function(num) computer.beep(num) end;
-        energy = function() return computer.energy() end;
-        freeMemory = function() return computer.freeMemory() end;
-        getArchitectures = function() return computer.getArchitectures() end;
-        getArchitecture = function() return computer.getArchitecture() end;
-        getBootAddress = function() return UUIDS[2] end;
-        getDeviceInfo = function() return computer.getDeviceInfo() end;
-        getProgramLocations = function() return VMF.ProgramLoactions end;
-        isRobot = function() return false end;
-        maxEnergy = function() return computer.maxEnergy() end;
-        
+VMF = {
+    ['addcall'] = function(name,...) table.insert(Calls,{name,...}) end;
+    ['computer'] = {
+        address = function()             return UUIDS[1] end;
+        addUser = function(name)         return false end;
+        beep = function(num)             computer.beep(num) end;
+        energy = function()              return computer.energy() end;
+        freeMemory = function()          return computer.freeMemory() end;
+        totalMemory = function()         return VMF.computer.freememory() end;
+        getArchitectures = function()    return computer.getArchitectures() end;
+        getArchitecture = function()     return computer.getArchitecture() end;
+        getBootAddress = function()      return UUIDS[2] end;
+        setBootAddress = function(addr)  VMF.addcall('setBootAddress',addr) return true end;
+        getDeviceInfo = function()       return computer.getDeviceInfo() end;
+        getProgramLocations = function() return {} end;
+        isRobot = function()             return false end;
+        energy = function()              return 500 end;
+        maxEnergy = function()           return computer.maxEnergy() end;
+        tmpAddress = function()          return UUIDS[3] end;
+        uptime = function()              return 100 end;
+        shutdown = function(reboot)      VMF.addcall('computer shutdown','reboot? '..tostring(reboot)) end;
     };
-    pcall = function(f,...) table.insert(Calls,{'pcall',f,...}) end;
-    
-}
-vmlua.transfer('VMLuaJAV',VMF,'VMF')
-local output
+    ['io'] = {
 
+    };
+    ['uuid'] = {
+        ['next'] = function() return uuid.next() end;
+    };
+    ['pcall'] = function(f,...) table.insert(Calls,{'pcall',f,...}) end;
+}
+VMF.require = function(module) VMF.addcall('require',module) return VMF[module] end;
+
+vmlua.transfer('VMLuaJAV',VMF,'VMF')
 local suc,err = pcall(function()
     return load(VMcode..'\n'..code)()
 end)
 vmlua.destroy('VMLuaJAV')
---print('\nHost: '..computer.address())
-for i,v in pairs(Calls[1]) do print(i,v) end
-print(suc,err,output)
+for i,v in pairs(Calls) do for _,b in pairs(v) do print('Call:',i,b) end end
+print(suc,err)
