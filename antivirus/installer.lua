48c5180b2bf6f05b03f1ffd2db4a38589382ed0a
just.acat <95175899+Jusxern@users.noreply.github.com>
2024-06-12 10:06:48 +0500

    Update installer.lua

--- antivirus/installer.lua
+++ antivirus/installer.lua
@@ -1,2 +1,14 @@
+local s = require('shell')
+local i = require('io')
 
-print('Installer running...')
+print('Insatlling JAV scan...')
+s.execute('wget -f https://raw.githubusercontent.com/Jusxern/oc-scripts/main/antivirus/scan.lua /bin/scan.lua')
+print('Installing VmLua package...')
+s.execute('wget -f https://raw.githubusercontent.com/Jusxern/oc-scripts/main/vmlua/vmlua.lua /lib/vmlua.lua')
+
+i.write('Restart PC now? Y/n\n')
+local input = i.read()
+if input == '' or input == 'Y' or input == 'y' then
+    s.execute('reboot')
+end
+print('Returning to Shell...')
