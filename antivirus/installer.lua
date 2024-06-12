a7b216f0c157755f4e558986c4c9c025552b09e3
just.acat <95175899+Jusxern@users.noreply.github.com>
2024-06-12 10:11:25 +0500

    Update installer.lua

--- antivirus/installer.lua
+++ antivirus/installer.lua
@@ -3,8 +3,8 @@ local i = require('io')
 
 print('Insatlling JAV scan...')
 s.execute('wget -f https://raw.githubusercontent.com/Jusxern/oc-scripts/main/antivirus/scan.lua /bin/scan.lua')
-print('Installing VmLua package...')
-s.execute('wget -f https://raw.githubusercontent.com/Jusxern/oc-scripts/main/vmlua/vmlua.lua /lib/vmlua.lua')
+print('Installing IOVLua package...')
+s.execute('wget -f https://raw.githubusercontent.com/Jusxern/oc-scripts/main/iovlua/iov.lua /lib/iov.lua')
 
 i.write('Restart PC now? Y/n\n')
 local input = i.read()
